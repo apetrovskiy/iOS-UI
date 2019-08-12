af35b65f1757e2afa8195416bbf9479d6156790d
Alexander <apetrovskiy@hotmail.com>
2019-08-12 10:15:56 +0300

    the initial state

---
+++ Lesson_01/Lesson_01/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Lesson_01
+//
+//  Created by Alexander Petrovskiy on 11/08/2019.
+//  Copyright © 2019 Alexander Petrovskiy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
